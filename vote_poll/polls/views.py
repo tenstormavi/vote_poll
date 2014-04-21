11d3f1cf225ef1f3459d14ba5bce674375ad0ef0
tenstormavi <avi.avinash3008@gmail.com>
2014-04-21 20:42:07 +0530

    Fixed typo

--- vote_poll/polls/views.py
+++ vote_poll/polls/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
@@ -9,10 +11,9 @@ from polls.models import Choice, Poll
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
         return Poll.objects.filter(
         pub_date__lte=timezone.now()
     ).order_by('-pub_date')[:5]
@@ -24,6 +25,7 @@ def home(request):
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
+    
     def get_queryset(self):
         return Poll.objects.filter(pub_date__lte=timezone.now())
 
@@ -46,4 +48,7 @@ def vote(request, poll_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls_results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+def add(request, poll_id):
+    p = get_object_or_404(Poll, pk = poll_id)
